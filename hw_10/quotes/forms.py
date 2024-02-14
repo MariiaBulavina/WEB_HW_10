143cb7ef5da1fcfa0f06fc044a976c0f579963ad
Mariia Bulavina <120931054+MariiaBulavina@users.noreply.github.com>
2024-02-13 22:31:57 -0600

    added the ability to add new authors and quotes

---
+++ hw_10/quotes/forms.py
@@ -0,0 +1,34 @@
+from django.forms import ModelForm, CharField, TextInput, DateField, ModelChoiceField, ModelMultipleChoiceField, CheckboxSelectMultiple
+from .models import Author, Tag, Quote
+
+
+class QuoteForm(ModelForm):
+    
+    quote = CharField()
+    tags = ModelMultipleChoiceField(queryset=Tag.objects.all(), widget=CheckboxSelectMultiple)
+    author = ModelChoiceField(queryset=Author.objects.all())
+
+    class Meta:
+        model = Quote
+        fields = ['quote', 'tags', 'author']
+
+
+# class TagForm(ModelForm):
+
+#     name = CharField(min_length=3, max_length=50, required=True, widget=TextInput())
+
+#     class Meta:
+#         model = Tag
+#         fields = ['name']
+
+
+class AuthorForm(ModelForm):
+
+    fullname = CharField(max_length=100, required=True, widget=TextInput())
+    born_date = DateField()
+    born_location = CharField()
+    description = CharField()
+
+    class Meta:
+        model = Author
+        fields = ['fullname', 'born_date', 'born_location', 'description']
--- hw_10/quotes/views.py
+++ hw_10/quotes/views.py
@@ -1,13 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
-from .utils import get_mongodb
+from .forms import  AuthorForm, QuoteForm
+from .models import Tag, Author, Quote
+
 
 def main(request, page=1):
 
-    db = get_mongodb()
-    quotes = db.quotes.find()
+    quotes = Quote.objects.select_related('author').prefetch_related('tags').all().order_by('created_at')
     per_page = 10
     paginator = Paginator(list(quotes), per_page)
     quotes_on_page = paginator.page(page)
     return render(request, 'quotes/index.html', context={'quotes': quotes_on_page})
+
+
+@login_required
+def author(request):
+
+    if request.method == 'POST':
+
+        form = AuthorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='quotes:root')
+        else:
+            return render(request, 'quotes/add_author.html', {'form': form})
+
+    return render(request, 'quotes/add_author.html', {'form': AuthorForm()})
+
+
+@login_required
+def quote(request):
+
+    if request.method == 'POST':
+        
+        form = QuoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='quotes:root')
+        else:
+            return render(request, 'quotes/add_quote.html', {'form': form})
+
+    return render(request, 'quotes/add_quote.html', {'form': QuoteForm()})
+
+
+def author_info(request, name):
+
+    print(name)
+    author = get_object_or_404(Author, fullname=name)
+    return render(request, 'quotes/author_info.html', {'author': author})
